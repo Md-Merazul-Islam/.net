5d6c4df469fb4c8d69afca39a2ea3fa11b9aee5b
Md-Merazul-Islam <mdmerazul75@gmail.com>
2025-09-15 07:40:41 +0600

    added dictionary

--- Day-2/MyApp/Program.cs
+++ Day-2/MyApp/Program.cs
@@ -5,13 +5,14 @@ class Program
 {
   static void Main(string[] args)
   {
-    int[] a = { 1, 2, 3, 4, 5 };
-    for (int i = 0; i < a.Length; i++)
-    {
-      Console.Write(a[i] + " ");
-    }
-    Console.WriteLine();
+    // int[] a = { 1, 2, 3, 4, 5 };
+    // for (int i = 0; i < a.Length; i++)
+    // {
+    //   Console.Write(a[i] + " ");
+    // }
+    // Console.WriteLine();
 
-    TryParseUse.Run(args);
+    // TryParseUse.Run(args);
+    Dictionarys.RunDict(args);
   }
 }
