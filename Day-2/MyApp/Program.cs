d0187fc18d8847e4c991b7d1358127bddafde763
Md-Merazul-Islam <mdmerazul75@gmail.com>
2025-09-16 06:34:02 +0600

    up

--- Day-2/MyApp/Program.cs
+++ Day-2/MyApp/Program.cs
@@ -1,9 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 
 class Program
 {
-  static void Main(string[] args)
+  static async Task Main(string[] args)
   {
     // int[] a = { 1, 2, 3, 4, 5 };
     // for (int i = 0; i < a.Length; i++)
@@ -17,5 +18,13 @@ class Program
     // HashSets.RunHash(args);
     // Queues.RunQue(args);
     Stacks.RunStack(args);
+    var result = await GetDataAsync();
+    Console.WriteLine(result);
   }
+  static async Task<string> GetDataAsync()
+  {
+    await Task.Delay(2000); // ২ সেকেন্ড delay
+    return "Data Loaded!";
+  }
+
 }
